a6ef3a4b38fa4221c20321f976d7c661e15f8a91
wangchen <2090861117@qq.com>
2020-03-08 22:43:33 +0800

    对数组, 对象, 类的约束

--- src/tsLearn/interfaceType.ts
+++ src/tsLearn/interfaceType.ts
@@ -22,3 +22,49 @@ interface IFullInfo {
 export function printOptionInfo(info: IFullInfo): void {
   console.log(info.firstName + (info.lastName || ''));
 }
+
+// 函数类型接口
+interface IEncrypt {
+  (key: string, value: string): string;
+}
+
+export const md5: IEncrypt = function(key: string, value: string): string {
+  return key + value;
+};
+
+md5('aaa', 'bbb');
+
+// 可索引接口 (数组, 对象的约束)
+// 数组
+interface UserArray {
+  [index: number]: string;
+}
+
+const userarr: UserArray = ['aa', 'bbb'];
+console.log(userarr);
+
+// 对象
+interface IUserObj {
+  [index: string]: string;
+}
+
+export const userObjArr: IUserObj = { name: '20' };
+
+// 类类型接口  约束 类
+interface Animal {
+  name: string;
+  eat(str: string): void;
+}
+
+export class Dog implements Animal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(this.name + '吃粮食');
+  }
+}
+
+
